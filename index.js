975d4675f01b44059c856558141fd8c464d0c88b
adryan0302 <dryan_guns@yahoo.co.id>
2023-06-09 13:18:45 +0700

    add start

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello Worlds!')
-  })
+})
 
-  app.get('/test', (req, res) => {
-    res.send('Hello test!')
-  })
+app.get('/test', (req, res) => {
+res.send('Hello test!')
+})
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Example app listening on port 3000`)
